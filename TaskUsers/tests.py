da4c4689269d54f3b87a31fc8976c65275521807
AKASHX5 <akashnsu2016@gmail.com>
2020-01-25 02:24:34 +0600

    user Log_in feature added

--- TaskUsers/tests.py
+++ TaskUsers/tests.py
@@ -2,8 +2,18 @@ from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
+import json
+import base64
 
 PASSWORD = 'cft6!'
+
+
+def create_user(username='user@example.com', password=PASSWORD):
+    """Helper function to keep our code DRY."""
+    return get_user_model().objects.create_user(
+        username=username,
+        password=password,
+    )
 
 
 class AuthenticationTest(APITestCase):
@@ -18,3 +28,24 @@ class AuthenticationTest(APITestCase):
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(response.data['id'], user.id)
         self.assertEqual(response.data['username'], user.username)
+
+    def test_user_can_log_in(self): # new
+        user = create_user()
+        response = self.client.post(reverse('log_in'), data={
+            'username': user.username,
+            'password': PASSWORD,
+        })
+
+        # Parse payload data from access token.
+        access = response.data['access']
+        header, payload, signature = access.split('.')
+        decoded_payload = base64.b64decode(f'{payload}==')
+        payload_data = json.loads(decoded_payload)
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertIsNotNone(response.data['refresh'])
+        self.assertEqual(payload_data['id'], user.id)
+        self.assertEqual(payload_data['username'], user.username)
+
+
+
--- TaskUsers/urls.py
+++ TaskUsers/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import SignUpView
-
+from .views import SignUpView, LoginView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
 
     path('sign_up/', SignUpView.as_view(), name='sign_up'),
+    path('log_in/', LoginView.as_view(), name='log_in'),
+    path('token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
 ]
